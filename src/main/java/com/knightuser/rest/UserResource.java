f5473c10bcc53ab4743cd2f8aa372834218ba142
Maksym <maksym.fedosov@avispl.com>
2020-04-03 21:14:28 +0300

    removed @validated annotation

--- src/main/java/com/knightuser/rest/UserResource.java
+++ src/main/java/com/knightuser/rest/UserResource.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(USERS_BASE_URL)
